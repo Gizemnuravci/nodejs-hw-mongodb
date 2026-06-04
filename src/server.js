6e08bf6dfba9f649925b4eaec4e6555eb3a6cbe0
Gizemnur Avcı <gizemnuravci4@gmail.com>
2026-06-04 23:24:16 +0300

    fix: resolve render deployment error

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ export const setupServer = () => {
 
   const PORT = process.env.PORT || 3000;
 
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
